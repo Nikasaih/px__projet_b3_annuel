547c6279ac47ec826e92eb3490f9427932e2e005
Nikasaih <gchaducnguyen@gmail.com>
2022-01-07 18:55:14 +0100

    ajout des fichier de base + ajout de swagger (doc generator)

---
+++ backend/src/main/java/b3/projetpro/backend/controller/TestController.java
@@ -0,0 +1,14 @@
+package b3.projetpro.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String miguel(){
+        return "test Get";
+    }
+}
