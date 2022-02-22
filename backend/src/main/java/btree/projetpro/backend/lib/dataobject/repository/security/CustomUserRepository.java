f1a6badb9089e4a51351eeb10e4216526104e867
Nikasaih <gchaducnguyen@gmail.com>
2022-02-22 07:05:28 +0100

    modification userrepo

--- backend/src/main/java/btree/projetpro/backend/lib/dataobject/repository/security/CustomUserRepository.java
+++ backend/src/main/java/btree/projetpro/backend/lib/dataobject/repository/security/CustomUserRepository.java
@@ -1,9 +1,9 @@
 package btree.projetpro.backend.lib.dataobject.repository.security;
 
 import btree.projetpro.backend.lib.dataobject.entity.security.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +11,8 @@ import java.util.Optional;
 
 @Repository
 @Transactional
-public interface CustomUserRepository extends JpaRepository<User, Long> {
+public interface CustomUserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
-
 
     @Transactional
     @Modifying
