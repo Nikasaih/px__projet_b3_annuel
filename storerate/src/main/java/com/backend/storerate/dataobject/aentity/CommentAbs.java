3abfa77255c6060a7b0003642cc73da290bb6c6c
Nikasaih <gchaducnguyen@gmail.com>
2022-05-10 06:40:58 +0200

    add javax validation on comment microservice

--- storerate/src/main/java/com/backend/storerate/dataobject/aentity/CommentAbs.java
+++ storerate/src/main/java/com/backend/storerate/dataobject/aentity/CommentAbs.java
@@ -4,13 +4,23 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @MappedSuperclass
 public abstract class CommentAbs {
+    @NotNull
+    @NotBlank
     private Long articlesId;
+    @NotNull
+    @NotBlank
     private Long userId;
+    @NotNull
+    @NotBlank
     private Float grade;
+    @NotNull
+    @NotBlank
     private String text;
 }
