fdbb3bfc458b157df5e27d0a4e3a14acc500bb49
Nikasaih <gchaducnguyen@gmail.com>
2022-05-11 21:46:42 +0200

    modification test

--- store/src/test/java/com/backend/store/controller/ArticleControllerTests.java
+++ store/src/test/java/com/backend/store/controller/ArticleControllerTests.java
@@ -33,7 +33,7 @@ public class ArticleControllerTests {
     private String DELETE_ONE_ROUTE;
 
     @BeforeEach
-      void setup() {
+    void setup() {
         String baseUrl = "http://localhost:" + port;
         CREATE_ONE_ROUTE = baseUrl + "/api/articles";
         UPDATE_ONE_ROUTE = CREATE_ONE_ROUTE;
@@ -43,7 +43,7 @@ public class ArticleControllerTests {
     }
 
     @Test
-      void test_getAll() {
+    void test_getAll() {
         log.info("test_getAll");
         ResponseEntity<ArticleSqlEntity[]> response = template.getForEntity(GET_ALL_ROUTE, ArticleSqlEntity[].class);
         Assertions.assertEquals(HttpStatus.OK.value(), response.getStatusCode().value());
@@ -53,14 +53,14 @@ public class ArticleControllerTests {
     //getOne
     @Test
     @Order(3)
-      void test_getOne_Exist() {
+    void test_getOne_Exist() {
         log.info("test_getOne_Exist");
         ResponseEntity<ArticleSqlEntity> response = template.getForEntity(GET_ONE_ROUTE + articleId, ArticleSqlEntity.class);
         Assertions.assertEquals(HttpStatus.OK.value(), response.getStatusCode().value());
     }
 
     @Test
-      void test_getOne_NotExist() {
+    void test_getOne_NotExist() {
         log.info("test_getOne_NotExist");
         ResponseEntity<ArticleSqlEntity> response = template.getForEntity(GET_ONE_ROUTE + "81515161", ArticleSqlEntity.class);
         Assertions.assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode().value());
@@ -68,35 +68,35 @@ public class ArticleControllerTests {
 
     @Test
     @Order(1)
-      void test_createOne() {
+    void test_createOne() {
         ArticleDto articleDto = PersistentUtility.defaultArticleDto();
-        ResponseEntity<?> response = template.postForEntity(CREATE_ONE_ROUTE, articleDto, ArticleSqlEntity.class);
+        ResponseEntity<?> response = template.postForEntity(CREATE_ONE_ROUTE, articleDto, Object.class);
         Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatusCode().value());
     }
 
     @Test
-      void test_createOne_badBinding() {
+    void test_createOne_badBinding() {
         ArticleDto articleDto = PersistentUtility.defaultArticleDto();
         articleDto.setMaterialsId(null);
-        ResponseEntity<String> response = template.postForEntity(CREATE_ONE_ROUTE, articleDto, String.class);
+        ResponseEntity<Object> response = template.postForEntity(CREATE_ONE_ROUTE, articleDto, Object.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCode().value());
     }
 
     @Test
     @Order(2)
-      void test_updateOne() {
+    void test_updateOne() {
         ArticleDto articleDto = PersistentUtility.defaultArticleDto();
         articleDto.setId(articleId);
-        ResponseEntity<ArticleSqlEntity> response = template.postForEntity(UPDATE_ONE_ROUTE, articleDto, ArticleSqlEntity.class);
+        ResponseEntity<Object> response = template.postForEntity(UPDATE_ONE_ROUTE, articleDto, Object.class);
         Assertions.assertEquals(HttpStatus.ACCEPTED.value(), response.getStatusCode().value());
     }
 
     @Test
     @Order(2)
-      void test_updateOne_badBinding() {
+    void test_updateOne_badBinding() {
         ArticleDto articleDto = PersistentUtility.faultArticleDto();
         articleDto.setId(articleId);
-        ResponseEntity<?> response = template.postForEntity(UPDATE_ONE_ROUTE, articleDto, Object.class);
+        ResponseEntity<Object> response = template.postForEntity(UPDATE_ONE_ROUTE, articleDto, Object.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCode().value());
     }
 
@@ -112,7 +112,7 @@ public class ArticleControllerTests {
     // delete by id 404
     @Test
     @Order(11)
-      void test_deleteOneById_NotExist() {
+    void test_deleteOneById_NotExist() {
         ResponseEntity<String> response = template.exchange(DELETE_ONE_ROUTE, HttpMethod.DELETE, null, String.class, 81515161L);
         Assertions.assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode().value());
     }
