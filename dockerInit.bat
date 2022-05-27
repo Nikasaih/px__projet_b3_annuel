97c15ee7874a2e4374251e37803a225f7ae317a6
Nikasaih <gchaducnguyen@gmail.com>
2022-05-27 14:15:30 +0200

    fix: bug volume elk

--- dockerInit.bat
+++ dockerInit.bat
@@ -1,7 +1,8 @@
 docker volume create backend
+docker volume create elk
 docker run --name=mysql1 -p 3307:3306  -v backend:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e  MYSQL_DATABASE=prod_sdv_bdd -d mysql:5.7
 
-docker run -d --name elasticsearch3 -v /srv/elas1:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "ELASTICSEARCH_HEAP_SIZE=512m" bitnami/elasticsearch
+docker run -d --name elasticsearch3 -v elk:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "ELASTICSEARCH_HEAP_SIZE=512m" bitnami/elasticsearch
 
 docker build --tag store_micro ./store
 docker build --tag gw_micro ./securitygw
